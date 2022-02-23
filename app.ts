9fe92457e15c413b3445b76d6c0f0edcf47377d0
Omar Abdulbagi <ramoboy56@gmail.com>
2022-02-23 22:06:14 +0300

    initial (backend)

---
+++ app.ts
@@ -0,0 +1,21 @@
+import express from 'express'
+import cors from 'cors'
+import path from 'path'
+import getStatus from './Routes/getStatus'
+import getJumpIssues from './Routes/getJumpIssues'
+
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.set('etag', false)
+// routes
+app.use(getStatus)
+app.use(getJumpIssues)
+if (process.env.NODE_ENV === 'production') {
+	const publicPath = path.join(__dirname, '../public')
+	app.use(express.static(publicPath))
+	app.use('*', express.static(publicPath))
+}
+
+export default app
